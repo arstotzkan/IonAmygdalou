9ce0a52bc4ec637d0475f166a745acefb1f9a8e4
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-05-09 20:07:09 +0300

    major update on viewmodel and uiState

--- src/AC-Remote-App/app/src/main/java/aueb/mlp/ac/MainActivityUiState.kt
+++ src/AC-Remote-App/app/src/main/java/aueb/mlp/ac/MainActivityUiState.kt
@@ -5,14 +5,26 @@ enum class Menu {
 }
 
 enum class Mode {
-    HEAT, COLD, // ...
+    HEAT, COLD, DRY, AUTO
+}
+
+enum class Fan {
+    SILENT, NORMAL, TURBO,
+}
+
+enum class Blinds {
+    HORIZONTAL, VERTICAL, FOLLOW_ME, OFF
 }
 // ...
 
 data class MainActivityUiState(
     val error: String = "",
     val activeMenu: Menu = Menu.MODE,
+    val acIsOn: Boolean = true,
     val temperature: Int = 25,
     val mode: Mode = Mode.HEAT,
+    val fan: Fan = Fan.NORMAL,
+    val blinds: Blinds = Blinds.OFF,
+    val ecoMode: Boolean = false
     // ...
 )
--- src/AC-Remote-App/app/src/main/java/aueb/mlp/ac/MainActivityViewModel.kt
+++ src/AC-Remote-App/app/src/main/java/aueb/mlp/ac/MainActivityViewModel.kt
@@ -5,7 +5,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import aueb.mlp.ac.model.ACFan
 import aueb.mlp.ac.model.ACMode
+import aueb.mlp.ac.model.ACBlinds
 import aueb.mlp.ac.model.AirConditioner
 
 class MainActivityViewModel(
@@ -15,6 +17,13 @@ class MainActivityViewModel(
     var uiState: MainActivityUiState by mutableStateOf(MainActivityUiState())
         private set
 
+
+    fun toggleOnOff(){
+        airConditioner.toggleOnOff()
+        uiState = uiState.copy(
+            acIsOn = !uiState.acIsOn
+        )
+    }
     fun incrementTemperature() {
         val success = airConditioner.incrementTemperature()
 
@@ -42,7 +51,8 @@ class MainActivityViewModel(
             when(mode) {
                 "HEAT" -> ACMode.HEAT
                 "COLD" -> ACMode.COLD
-                // ...
+                "DRY" -> ACMode.DRY
+                "AUTO" -> ACMode.AUTO
                 else -> error("Invalid mode: $mode")
             }
         )
@@ -51,10 +61,57 @@ class MainActivityViewModel(
             mode = when(airConditioner.acMode) {
                 ACMode.HEAT -> Mode.HEAT
                 ACMode.COLD -> Mode.COLD
-                // ...
+                ACMode.DRY -> Mode.DRY
+                ACMode.AUTO -> Mode.AUTO
             }
         )
     }
 
+    fun setFan (fan : String){
+        airConditioner.setFan(
+            when(fan) {
+                "SILENT" -> ACFan.SILENT
+                "NORMAL" -> ACFan.NORMAL
+                "TURBO" -> ACFan.TURBO
+                else -> error("Invalid fan mode: $fan")
+            }
+        )
+
+        uiState = uiState.copy(
+            fan = when(airConditioner.acFan) {
+                ACFan.SILENT -> Fan.SILENT
+                ACFan.NORMAL -> Fan.NORMAL
+                ACFan.TURBO -> Fan.TURBO
+            }
+        )
+    }
+
+    fun setBlinds (blinds: String){
+        airConditioner.setBlinds(
+            when(blinds) {
+                "HORIZONTAL" -> ACBlinds.HORIZONTAL
+                "VERTICAL" -> ACBlinds.VERTICAL
+                "FOLLOW ME" -> ACBlinds.FOLLOW_ME
+                "OFF" -> ACBlinds.OFF
+                else -> error("Invalid blind mode: $blinds")
+            }
+        )
+
+        uiState = uiState.copy(
+            blinds = when(airConditioner.blinds) {
+                ACBlinds.HORIZONTAL -> Blinds.HORIZONTAL
+                ACBlinds.VERTICAL -> Blinds.VERTICAL
+                ACBlinds.FOLLOW_ME -> Blinds.FOLLOW_ME
+                ACBlinds.OFF -> Blinds.OFF
+            }
+        )
+    }
+
+    fun toggleEcoMode(){
+        airConditioner.toggleEcoMode()
+        uiState = uiState.copy(
+            ecoMode = !uiState.ecoMode
+        )
+    }
     // ...
 }
